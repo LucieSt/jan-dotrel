033b4a4d72b118c52bc374cbba90b5f41b4e0878
Filip Bouska <bouska.filip@gmail.com>
2020-03-01 22:19:17 +0100

    force latest

--- text.php
+++ text.php
@@ -22,15 +22,22 @@
         <div class="row texts">
             <div class="col-md-4">
                 <a href="text1">
-                    <img src="images/text/thumbs/01.jpg" class="portfolio-image text1">
-                    <span class="extracts-title">MIROSLAV PETŘÍČEK / VTEŘINY SYMETRIE</span>
+                    <img src="images/text/01-min.jpg" class="portfolio-image text1">
+                    <span class="extracts-title">JOZEF MRVA ML. / DEEPLY ORDERED CHAOS</span>                    
                 </a>
             </div>
 
             <div class="col-md-4">
                 <a href="text2">
-                    <img src="images/text/thumbs/02.jpg" class="portfolio-image text2">
-                    <span class="extracts-title">DEEPLY ORDERED CHAOS</span>
+                    <img src="images/text/02-min.jpg" class="portfolio-image text2">
+                    <span class="extracts-title">MIROSLAV PETŘÍČEK / VTEŘINY SYMETRIE</span>
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="text3">
+                    <img src="images/text/03-min.jpg" class="portfolio-image text3">
+                    <span class="extracts-title">JAN DOTŘEL / GRID</span>
                 </a>
             </div>
         </div>
@@ -42,15 +49,21 @@
 <script>
 
     $('.text1').hover(function(){
-        $('.text1').attr('src', 'images/text/thumbs/01_box.jpg')
+        $('.text1').attr('src', 'images/text/01-box-min.jpg')
     }, function(){
-        $('.text1').attr('src', 'images/text/thumbs/01.jpg')
+        $('.text1').attr('src', 'images/text/01-min.jpg')
     });
 
     $('.text2').hover(function(){
-        $('.text2').attr('src', 'images/text/thumbs/02_box.jpg')
+        $('.text2').attr('src', 'images/text/02-box-min.jpg')
     }, function(){
-        $('.text2').attr('src', 'images/text/thumbs/02.jpg')
+        $('.text2').attr('src', 'images/text/02-min.jpg')
+    });
+
+     $('.text3').hover(function(){
+        $('.text3').attr('src', 'images/text/03-box-min.jpg')
+    }, function(){
+        $('.text3').attr('src', 'images/text/03-min.jpg')
     });
 
 </script>
